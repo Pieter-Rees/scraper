d834b6ddfc02a8c4bdfef7532c9a2139b086a1d8
Pieter Rees <pieter@pieterrees.nl>
2022-09-04 20:28:49 +0000

    added workers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import worker_script from "./src/Worker.js";
+import worker_script from "./Worker.js";
 import "./App.css";
 
 class MyClass extends React.Component {
+  state = {};
+
   constructor() {
     super();
-
     this.state = { clickCount: 1 };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -13,13 +14,14 @@ class MyClass extends React.Component {
   componentDidMount() {
     this.worker = new Worker(worker_script);
     this.worker.onmessage = ev => {
-      console.log("got data back from worker");
+      console.log("got data back from worker (4)");
       console.log(ev);
     };
+
   }
 
   handleClick() {
-    console.log("Posting message");
+    console.log("Posting message (1)");
     this.worker.postMessage("Button clicked");
   }
 
